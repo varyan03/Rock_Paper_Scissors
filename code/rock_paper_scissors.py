721b8c3b22c0b4544a92f4f03a80e612b15473fd
Aryan <av868600@gmail.com>
2024-06-24 16:23:45 +0530

    added while loop

--- code/rock_paper_scissors.py
+++ code/rock_paper_scissors.py
@@ -7,3 +7,9 @@ options=["rock","paper","scissors"]
 #variables to keep a count of wins of player and comp
 comp_wins=0
 user_wins=0
+
+while True:
+
+    #generating a interger for the index of the options ,so that a random option can be assigned 
+    rand_num=random.randint(0,2)
+    comp_choice=options[rand_num]
