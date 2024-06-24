bb5ebb03679b608f75749d20e7d52df6d8dc4b0c
Aryan <av868600@gmail.com>
2024-06-24 16:22:39 +0530

    created var

---
+++ code/rock_paper_scissors.py
@@ -0,0 +1,9 @@
+#importing random module 
+import random
+
+#stored the three possible options in a list
+options=["rock","paper","scissors"]
+
+#variables to keep a count of wins of player and comp
+comp_wins=0
+user_wins=0
